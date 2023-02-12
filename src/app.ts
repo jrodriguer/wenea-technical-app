5408f6c1dc9cb53c31e324e6880d9cc531ee7dc6
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2023-02-12 19:46:54 +0100

    install firebase dependency and write the base of endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,35 @@
 import express from 'express';
+import firebase from 'firebase';
 
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDXWEC_FNsxqHaHiqIipKjoMmf16nhaMzM",
+  authDomain: "wenea-thechnical.firebaseapp.com",
+  databaseURL: "https://wenea-thechnical-default-rtdb.firebaseio.com",
+  projectId: "wenea-thechnical",
+  storageBucket: "wenea-thechnical.appspot.com",
+  messagingSenderId: "449693082463",
+  appId: "1:449693082463:web:045a2f70101f0b601b4cea",
+  measurementId: "G-KZM6M2T4V1"
+};
+firebase.initializeApp(firebaseConfig);
+
+// User registration endpoint
+app.post('/register', (req, res) => {
+  // Add code to handle user registration
+});
+
+// User login endpoint
+app.post('/login', (req, res) => {
+  // Add code to handle user login
+});
+
+// User edit endpoint
+app.put('/edit', (req, res) => {
+  // Add code to handle user editing
 });
 
 app.listen(port, () => {
