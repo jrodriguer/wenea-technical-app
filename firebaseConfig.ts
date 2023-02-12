5893ea48c39dc7b56cae196caf6332c04af26873
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2023-02-13 00:05:46 +0100

    ensure that sensitive information with environment variables and dotenv library

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,4 +1,7 @@
 import * as admin from 'firebase-admin';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const privateKey = JSON.parse(process.env.FIREBASE_PRIVATE_KEY!);
 const databaseURL = process.env.FIREBASE_DATABASE_URL!;
